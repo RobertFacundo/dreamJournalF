9d9d90d7b218e251509aa74d199884a05d5fa801
Facundo Robert <robert.facundo@hotmail.com>
2025-06-21 15:01:50 -0300

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Profile } from './views/Profile';
 import { useState } from 'react';
 import { FixedBackground } from './components/Background/FixedBackground';
 import { ParallaxBackground } from './views/ParallaxBackground';
+import { Footer } from './components/Home/Footer';
 import './app.scss';
 import { Auth } from './views/Authentication';
 
@@ -23,9 +24,10 @@ function App() {
           <FixedBackground moonOpacity={1} onMoonClick={isHome ? () => { console.log('🌕 Luna clickeada'); setShowMoonModal(true) } : undefined} />
           <Routes>
             <Route path='/' element={<Auth />} />
-            <Route path='/Home' element={<Home showMoonModal={showMoonModal} setShowMoonModal={setShowMoonModal}/>} />
+            <Route path='/Home' element={<Home showMoonModal={showMoonModal} setShowMoonModal={setShowMoonModal} />} />
             <Route path='/Profile' element={<Profile />} />
           </Routes>
+          <Footer />
         </>
       )}
     </>
